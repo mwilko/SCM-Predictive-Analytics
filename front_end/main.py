853d9ef7c6cc8e2d1c48f7ef340f978b8cd99ea1
Max Wilkinson <mjswilkinson@outlook.com>
2025-03-18 14:37:08 +0000

    adding data expander to the front-end

--- front_end/main.py
+++ front_end/main.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.write('hello world')
+st.title('Predictive Analytics for Supply Chain Management')
+
+st.info('This app uses machine learning models to predict a certain criteria')
+
+with st.expander('Data'):
+    st.write('**Processed Dataset**')
+    product_sales = pd.read_cv('..\datasets\forecasting\final\product_sales.csv')
+    product_sales.head()
+
+    st.write('**X**')
+    X = product_sales.drop('OrderQuantity', axis=1)
+
+    st.write('**y**')
+    y = product_sales.OrderQuantity
